77cfc388665e11d1f36b6b55abf066f73688d70f
Dhairya Thareja <dhairyathareja@gmail.com>
2025-07-07 09:46:12 +0530

    Deployment Changes

--- src/index.js
+++ src/index.js
@@ -26,6 +26,14 @@ app.use(bodyParser.json({ limit: '5KB' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: "5KB" }));
 app.use(cookieParser());
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+// For SPA (React) — send index.html for any unknown route
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
